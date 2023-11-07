f0f0996c337417c33c0fca63d072ce1fc3841f43
Yash Gupta <gyash21@gmail.com>
2023-11-07 23:42:40 +0530

    update- rendered outline button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
 
 export default function Home() {
   return (
-    <p>Hello Yotion</p>
+    <div>
+<Button variant="outline">
+  Click Me
+</Button>
+    </div>
   )
 }
